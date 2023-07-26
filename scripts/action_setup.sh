1238c5d7d2d2bc9b600544d11ae2d8cea36b1924
Jon Gallant <2163001+jongio@users.noreply.github.com>
2023-07-26 23:39:06 +0000

    Try to set slot name as it was defaulting to production

--- scripts/action_setup.sh
+++ scripts/action_setup.sh
@@ -24,10 +24,10 @@ apiPublishProfile=$(az webapp deployment list-publishing-profiles --name $apiApp
 
 # Write API App publish profile to file
 mkdir -p .publish
-echo $apiPublishProfile > .publish/api_publish_profile.json
+echo $apiPublishProfile > .publish/api_publish_profile.PublishSettings
 
 # Set API App publish profile as GitHub secret
-gh secret set AZURE_WEBAPP_PUBLISH_PROFILE_API -b"$(cat .publish/api_publish_profile.json)"
+gh secret set AZURE_WEBAPP_PUBLISH_PROFILE_API -b"$(cat .publish/api_publish_profile.PublishSettings)"
 
 # Set API App name as GitHub variable
 gh variable set AZURE_WEBAPP_NAME_API --body "$apiAppName"
@@ -36,10 +36,10 @@ gh variable set AZURE_WEBAPP_NAME_API --body "$apiAppName"
 webPublishProfile=$(az webapp deployment list-publishing-profiles --name $webAppName --resource-group $resourceGroup --xml --output tsv)
 
 # Write Web App publish profile to file
-echo $webPublishProfile > .publish/web_publish_profile.json
+echo $webPublishProfile > .publish/web_publish_profile.PublishSettings
 
 # Set Web App publish profile as GitHub secret
-gh secret set AZURE_WEBAPP_PUBLISH_PROFILE_WEB -b"$(cat .publish/web_publish_profile.json)"
+gh secret set AZURE_WEBAPP_PUBLISH_PROFILE_WEB -b"$(cat .publish/web_publish_profile.PublishSettings)"
 
 # Set Web App name as GitHub variable
 gh variable set AZURE_WEBAPP_NAME_WEB --body "$webAppName"
